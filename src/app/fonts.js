3cef9b2f7b126f0358f3e3d6b96e31ded68ea369
Anastasiia Polovynko <nastenka@Nastenkas-MacBook-Air.local>
2024-09-26 00:41:32 +0300

    init: basic app config

---
+++ src/app/fonts.js
@@ -0,0 +1,6 @@
+import { Onest } from 'next/font/google';
+
+export const onest = Onest({
+   weight: ['400', '500', '700'],
+   subsets: ['cyrillic']
+});
---
+++ src/app/layout.js
@@ -0,0 +1,18 @@
+import { onest } from './fonts';
+import './styles/style.css';
+
+export const metadata = {
+  title: 'GET_SHOP_TV',
+  description: 'One pager with Next.js',
+};
+
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={onest.className}>
+        {children}
+      </body>
+    </html>
+  );
+}
