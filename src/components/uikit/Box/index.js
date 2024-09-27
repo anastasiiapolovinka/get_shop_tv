a995333fa53ce525ef4d1b8b3a65656a997a8713
Anastasiia Polovynko <nastenka@Nastenkas-MacBook-Air.local>
2024-09-27 17:47:55 +0300

    feat: extand Box props

--- src/components/uikit/Box/index.js
+++ src/components/uikit/Box/index.js
@@ -1,7 +1,25 @@
-import styles from './index.module.css';
+import clsx from "clsx";
+import styles from "./index.module.css";
 
-export const Box = ({ children, className, Component = 'div', ...props }) => (
-   <Component className={`${styles.box} ${className}`} {...props}>
-      {children}
-   </Component>
+export const Box = ({
+  children,
+  className,
+  Component = "div",
+  direction,
+  align,
+  justify,
+  ...props
+}) => (
+  <Component
+    className={clsx([
+      styles.box,
+      className,
+      direction === "column" && styles.column,
+      align && `align_${align}`,
+      justify && `justify_${justify}`,
+    ])}
+    {...props}
+  >
+    {children}
+  </Component>
 );
