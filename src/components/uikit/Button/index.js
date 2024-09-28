bbc68aab30d79c69d92dfb23d085467f93714653
Anastasiia Polovynko <polovinkanasta@gmail.com>
2024-09-28 15:54:30 +0300

    feat: use clsx util

--- src/components/uikit/Button/index.js
+++ src/components/uikit/Button/index.js
@@ -1,7 +1,23 @@
-"use client"
-import styles from './index.module.css';
+"use client";
+import clsx from "clsx";
+import styles from "./index.module.css";
 
-export const Button = ({ type = 'button', className, handleClick = () => {},  children, ...props }) => {
-   const btnClassName = `${styles.btn} ${className}`;
-   return <button onClick={handleClick} className={btnClassName} type={type} {...props}>{children}</button>;
-}
+export const Button = ({
+  type = "button",
+  className,
+  handleClick = () => {},
+  children,
+  ...props
+}) => {
+  const btnClassName = clsx([styles.btn, className]);
+  return (
+    <button
+      onClick={handleClick}
+      className={btnClassName}
+      type={type}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+};
