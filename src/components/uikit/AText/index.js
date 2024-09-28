c2b3f16824a96f6ddff57a821d947f71e5a4c918
Anastasiia Polovynko <polovinkanasta@gmail.com>
2024-09-28 15:53:44 +0300

    feat: extend AText styles + use clsx util

--- src/components/uikit/AText/index.js
+++ src/components/uikit/AText/index.js
@@ -1,7 +1,14 @@
-import styles from './index.module.css';
+import clsx from "clsx";
+import styles from "./index.module.css";
 
-export const AText = ({ children, className, Component = 'p', variant = 'txt_1', ...props }) => (
-   <Component className={`${styles[variant]} ${className}`} {...props}>
-      {children}
-   </Component>
+export const AText = ({
+  children,
+  className,
+  Component = "p",
+  variant = "txt_1",
+  ...props
+}) => (
+  <Component className={clsx([styles[variant], className])} {...props}>
+    {children}
+  </Component>
 );
